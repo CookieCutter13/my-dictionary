6755cec2c4903b12472149d80f83456386cd81ea
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-07-11 18:03:47 -0700

    add logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
-
-import './App.css';
+import logo from "./dic.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-   
+        <h1>Dictionary</h1>
+        <img src={logo} className="App-logo img-fuid" alt="logo" />
       </header>
     </div>
   );
