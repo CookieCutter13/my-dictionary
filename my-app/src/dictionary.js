2aa70f0de818a729fde8cdc9a80dca612c0670f6
CookieCutter13 <118944752+CookieCutter13@users.noreply.github.com>
2023-07-17 16:43:16 -0700

    log

--- my-app/src/dictionary.js
+++ my-app/src/dictionary.js
@@ -13,6 +13,7 @@ export default function Dictionary() {
 
  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/
     en_US/${keyword}`;
+   
     axios.get(apiUrl).then(handleResponse);
   }
 
